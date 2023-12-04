837dc1bcc7d31c16ed6ee5eed155184b5f0b2879
Niraj Kumar <118625808+niraj7680@users.noreply.github.com>
2023-12-04 11:32:49 +0000

    done

---
+++ basic01/variable01.js
@@ -0,0 +1,15 @@
+const accountId = 121344
+let accountEmail="nirajkumar@656.com"
+var accountpassword ="1234423"
+accountCity="Jaipur"
+
+//accountId=2// this is not allow
+console.log(accountId);
+
+accountEmail="nkie@hd.com"
+accountpassword="8865495"
+accountCity="new delhi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail,accountpassword,accountCity])
